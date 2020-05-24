df6c9a140c83679a6775abd7beadc650d803c29a
WahidX <wahidx93@gmai.com>
2020-05-24 14:36:43 +0530

    Added HelloAPIView

--- profiles_project/profiles_api/views.py
+++ profiles_project/profiles_api/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
-
+from rest_framework.views import APIView
+from rest_framework.response import Response
 # Create your views here.
+
+class HelloApiView(APIView):
+
+    def get(self, request, format=None):
+        dataList = ['Just','a','sample','list']
+
+        return Response({'message':'Hello X','data':dataList})
+        
